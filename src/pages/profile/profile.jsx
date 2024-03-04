73be63d68b6719eafa5fe708b4e3463567fc1c98
GuliciucDenis <Guliciuc.denis2003@gmail.com>
2024-03-04 22:02:58 +0200

    first commit

---
+++ src/pages/profile/profile.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../../components/Navbar/Navbar";
+import "./profile.css"
+
+export default function Profile(){
+    return(
+    <div className="wrapper">
+        <h2 className="Title" >My Account</h2>
+    </div>
+    )
+}
